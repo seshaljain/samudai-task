43316548614df42754af3856d8b7fba4ef2e610a
Seshal Jain <seshaljain11@gmail.com>
2022-11-09 16:16:41 +0530

    feat(routing): add react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
-import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Dashboard from './pages/Dashboard'
 
 function App() {
   return (
     <div>
-      Hello, world!
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
